6cd56bd8a5557668c3ddbf1c15344acb4f14347e
eroinnovations <eroinnovations9@gmail.com>
2025-05-28 20:46:26 +0300

    Updates

--- project.js
+++ project.js
@@ -4,16 +4,15 @@ const NOVASTART=()=>{
 
         <header>
 
-            <p class="LeftText">Ash Shakur Charity and Orphanage Aid</p>
+            <img class="CompanyLogo" src="https://ibrah9856.github.io/Ashshakurcharity/Assets/logo.jpg">
+        
+            <p class='CompanyName'>Ash Shakur Charity and Orphanage Aid</p>
 
             <img class="RightIcon" src="${WHITEMENUICON}"/>
 
         </header>
 
-        <img class="CompanyLogo" src="https://ibrah9856.github.io/Ashshakurcharity/Assets/logo.jpg">
-
-        <p>Test</p>
         
-    `)
+    `);
 
 };
