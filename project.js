4c4c02a26765c1b4940d78ec2b2eb24cb41ab474
eroinnovations <eroinnovations9@gmail.com>
2025-05-28 21:33:25 +0300

    Updates

--- project.js
+++ project.js
@@ -1,6 +1,12 @@
 const NOVASTART=()=>{
 
-    DISPLAY("",`
+   ROUTE('',HOMEPAGE,'HOMEPAGE');
+
+};
+
+const HOMEPAGE=()=>{
+
+        DISPLAY("",`
 
         <header>
 
@@ -8,10 +14,62 @@ const NOVASTART=()=>{
         
             <p class='CompanyName'>Ash Shakur Charity and Orphanage Aid</p>
 
-            <img class="RightIcon" src="${WHITEMENUICON}"/>
+            <img id='MenuOpen' onclick='MENUOPEN()' class="RightIcon" src="${WHITEMENUICON}"/>
+
+            <img id='MenuClose' onclick='MENUCLOSE()' class="RightIcon" src="${WHITECLOSEICON}"/>
 
         </header>
 
+        <div class='TopNav' id='Menu' >
+
+            <p>About Us</p>
+
+            <p>Our Projects</p>
+
+            <p>Contact Us</p>
+
+        </div>
+
     `);
 
+};
+
+const MENUOPEN=()=>{
+
+    var MENU=NAMING('#MenuOpen');
+
+    var MENUCLOSER=NAMING('#MenuClose');
+
+    var MENUDRAWER=NAMING('#Menu');
+
+    CLICK(MENU,()=>{
+
+        STYLED(MENUDRAWER,'display','inline-flex');
+
+        STYLED(MENUCLOSER,'display','block');
+
+        STYLED(MENU,'display','none');
+
+    });
+
+};
+
+const MENUCLOSE=()=>{
+
+    var MENU=NAMING('#MenuOpen');
+
+    var MENUCLOSER=NAMING('#MenuClose');
+
+    var MENUDRAWER=NAMING('#Menu');
+
+    CLICK(MENUCLOSER,()=>{
+
+        STYLED(MENUDRAWER,'display','none');
+
+        STYLED(MENUCLOSER,'display','none');
+
+        STYLED(MENU,'display','block');
+
+    });
+ 
 };
