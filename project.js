320bd1a8416b348caed608d2f3890dd5e0cffcb9
ibrah9856 <ashshakurcharityandorphanageai@gmail.com>
2025-05-28 16:13:51 +0300

    Updates

---
+++ project.js
@@ -0,0 +1,15 @@
+const NOVASTART=()=>{
+
+    DISPLAY("",`
+
+        <header>
+
+            <p class="LeftText">Ash shakur Charity and Orphanage Aid</p>
+
+            <img class="RightIcon" src="${WHITEMENUICON}"/>
+
+        </header>
+        
+    `)
+
+};
